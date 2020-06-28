1959b6389c53c9d013668422d35693cae8a8d273
Will Goldie <wgoldie@gmail.com>
2020-06-28 13:35:48 -0700

    ignore work

--- management/commands/__init__.py
+++ management/commands/__init__.py
@@ -7,14 +7,24 @@ from ..session import Session
 CommandFunc = Callable[[Session, List[str]], None]
 
 
-def add_board(session, arguments):
-    assert_arguments(arguments, 1)
-    name, = arguments
+def add_board(session: Session, arguments: List[str]):
+    name, = assert_arguments(arguments, 1)
     Query(
         session, 'insert into boards (name) values (%s);', (name,)
     ).run()
 
 
+def add_card(session: Session, arguments: List[str]):
+    board_name, card_name = assert_arguments(arguments, 2)
+    Query(session,
+    '''
+        with source as (select id, %s as name from boards where name = %s)
+        insert into cards (board_id, name) (select id, name from source);
+    ''', (card_name, board_name)
+    ).run()
+
+
 dispatch_map: Dict[str, CommandFunc] = {
-    'add_board': add_board
+    'add_board': add_board,
+    'add_card': add_card
 }
--- management/commands/util.py
+++ management/commands/util.py
@@ -1,9 +1,13 @@
+from typing import Tuple, Iterable
 from ..exceptions import UserError
 
-def assert_arguments(args, min_len: int, max_len: int = None):
+def assert_arguments(
+    args: Iterable[str], min_len: int, max_len: int = None
+) -> Tuple[str, ...]:
     if max_len is None:
         max_len = min_len
     if len(args) < min_len:
         raise UserError("Too few arguments")
     if len(args) > max_len:
         raise UserError("Too many arguments")
+    return tuple(args)
