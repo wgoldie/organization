daba0f446599ba35d9010687f0f4ba7aeb89aefa
Will Goldie <wgoldie@gmail.com>
2020-06-28 14:54:16 -0700

    more commands

--- management/commands/__init__.py
+++ management/commands/__init__.py
@@ -14,6 +14,14 @@ def add_board(session: Session, arguments: List[str]):
     ).run()
 
 
+def list_boards(session: Session, arguments: List[str]):
+    assert_arguments(arguments, 0)
+    with Query(session, 'select name from boards;') as cursor:
+        print('Boards:')
+        for name, in cursor:
+            print(f"- {name}")
+
+
 def add_card(session: Session, arguments: List[str]):
     board_name, card_name = assert_arguments(arguments, 2)
     Query(session,
@@ -24,7 +32,40 @@ def add_card(session: Session, arguments: List[str]):
     ).run()
 
 
+def list_cards(session: Session, arguments: List[str]):
+    board_name, = assert_arguments(arguments, 1)
+    
+    query = Query(session,
+                  '''select columns.name
+                  from columns, boards
+                  where columns.board_id = boards.id
+                  and boards.name = %s order by columns.index;''',
+                  (board_name,))
+    columns = {None: []}
+    with query as cursor:
+        for column_name, in cursor:
+            columns[column_name] = []
+
+    query = Query(session,
+                  '''select cards.name, columns.name
+                  from cards
+                  left join columns on columns.id = cards.column_id
+                  inner join boards on boards.id = cards.board_id
+                  and boards.name = %s;''',
+                  (board_name,))
+    with query as cursor:
+        print(f'{board_name} - Cards:')
+        for card_name, column_name in cursor:
+            columns[column_name].append(card_name)
+    for column_name, card_names in columns.items():
+        print(f'- {column_name}')
+        for card_name in card_names:
+            print(f'\t- {card_name}')
+
+
 dispatch_map: Dict[str, CommandFunc] = {
     'add_board': add_board,
-    'add_card': add_card
+    'list_boards': list_boards,
+    'add_card': add_card,
+    'list_cards': list_cards
 }
