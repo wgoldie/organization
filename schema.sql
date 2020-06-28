98853bf2afe1abb3ff5d2aef6b40fdeca8cd178a
Will Goldie <wgoldie@gmail.com>
2020-06-28 13:00:07 -0700

    work on schema

--- schema.sql
+++ schema.sql
@@ -1,13 +1,15 @@
 create table boards (
   id serial primary key,
-  name text
+  name text unique
 );
 
 create table columns (
   id serial primary key,
   name text,
   index integer,
-  board_id integer references boards (id)
+  board_id integer references boards (id),
+  unique (board_id, name),
+  unique (board_id, index)
 );
 
 create table cards (
@@ -15,15 +17,18 @@ create table cards (
   board_id integer references boards (id),
   column_id integer references columns (id),
   name text,
-  body text
+  body text,
+  unique (board_id, name)
 );
 
 create table tags (
   id serial primary key,
-  name text
+  name text unique
 );
 
 create table card_tags (
   id serial primary key,
-  card_id integer references cards (id)
+  card_id integer references cards (id),
+  tag_id integer references tags (id),
+  unique (card_id, tag_id)
 );
